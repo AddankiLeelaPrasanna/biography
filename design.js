b7d13cbd0451b6fad455caaa63c874a4010a292d
AddankiLeelaPrasanna <137883522+AddankiLeelaPrasanna@users.noreply.github.com>
2023-07-28 19:15:40 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,63 @@
+ReactDOM.render(
+    <div>
+         <h1>***************************Avul Pakir Jainulabdeen Abdul Kalam******************************</h1>
+         <img src="https://knisha.files.wordpress.com/2015/07/kalam.jpg" width="100%" height="100%"/>
+         <ul>
+            <li> DOB&PLACE: October 15, 1931, Rameswaram, India</li>
+            <li>DEATH:  July 27, 2015, Shillong</li>
+            <li>He was an Indian Scientist and also a politician leader, who later became the 11th President of India.</li>
+            <li>He played an important role in the development of India’s missile and nuclear weapons program.</li>
+         </ul>
+         <h4>-------------------------------------------------------------------------------------------------------------About His Family and Struggle Life--------------------------------------------------------------------------------------------------------------------</h4>
+        <ul>
+            <li>Dr. A P J Abdul Kalam was born to a poor Tamil Muslim family.</li>
+            <li>At a tender age, Kalam had to sell newspapers to supplement his family income.</li>
+        </ul>
+        <h4> ----------------------------------------------------------------------------------------------------------------------------Education----------------------------------------------------------------------------------------------------------------------------------</h4>
+        <ul>
+            <li>Early education in Schwartz higher secondary school.</li>
+            <li>Saint Joseph's College, he graduated in physics in 1954.</li>
+            <li>Aerospace engineering at Madras Institute of Technologyin 1955.</li>
+        </ul>
+        <h4>--------------------------------------------------------------------------------------------------------------------------Kalam as a Scientist---------------------------------------------------------------------------------------------------------------------------</h4>
+            <ul>
+            <li>Kalam joined as a scientist in the aeronautical development establishment of DRDO in 1960.</li>
+                <li>Kalam was transferred to ISRO in 1969, where he was the project director of India's first satellite vehicle launch.</li>
+                <li> His research and immense knowledge brought him and the nation great laurels in the 1980s.</li>
+                <li>Kamal put forward a nationwide plan called Technology Vision 2020, which according to him, was a fantastic way for transforming
+                     the stature of India in 20 years, taking it from a developing to a developed nation.</li>
+
+            </ul>
+            <h4>-----------------------------------------------------------------------------------------------------------------Kalam as the 11th President of India----------------------------------------------------------------------------------------------------------------</h4>
+            <ul>
+                <li>Sir Kalam was entitled to be the 11th president of India.</li>
+                <li> His term period of 25th July 2002 to 25th July 2007 was achieved by winning a
+                     presidential election in 2002 with a massive margin of votes.</li>
+                     <li>In 2012, he introduced a program called “What Can I Give?” focusing on the theme of eradicating corruption from the country.</li>
+            </ul>
+            <h4>-----------------------------------------------------------------------------------------------------------------Writings of Dr. APJ Abdul Kalam-------------------------------------------------------------------------------------------------------------------</h4>
+            <ol>
+                <li>India 2010</li>
+                <li>Touched off Minds</li>
+                <li>Mission India</li>
+                <li>The Luminous Sparks</li>
+                <li>Wings of Fire</li>
+                <li>Moving Thoughts</li>
+            </ol>
+            <h4>---------------------------------------------------------------------------------------------------------------Achievements of Abdul Kalam-------------------------------------------------------------------------------------------------------------------------</h4>
+            <ol>
+                <li>Padma Bhushan award in 1981</li>
+                <li>Padma Bhushan award in 1990</li>
+                <li> Bharat Ratna in 1997</li>
+                <li>Indira Gandhi Award for National Integration in 1997</li>
+                <li>Veer Savarkar award in 1998</li>
+                <li>SASTRA Ramanujan prize in 2000</li>
+                <li>Von Braun Award by the National Space Society in 2013</li>
+            </ol>
+            <img src="https://theknowledgereview.com/wp-content/uploads/2022/10/Dr-APJ-Abdul-Kalam-%E2%80%93-A-Complete-Teacher-and-an-Inspiring-President.jpg"width="100%" height="100%"/>
+            <h1>*******************************MISSILE MAN OF INDIA***********************************</h1>
+        
+    </div>
+   
+
+   ,document.getElementById('content'));
